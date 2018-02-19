43771b241a542905ad1268a6f8bb67a36c26bf4a
hebecked <dustin.hebecker@gmail.com>
2018-02-19 13:51:07 +0100

    Added the script

---
+++ extract.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python2.7
+
+import requests
+import os, sys
+import argparse
+import validators
+import xmltodict
+import string
+
+parser = argparse.ArgumentParser(description='This script is meant for the extraction of wiki content from the "Humansiten Wiki".')
+parser.add_argument('-p', '--pagename', dest='PAGENAME', default='Leitbild', action='store', type=str, help='Name of the wiki page to be extracted') # might use nargs='+' too
+
+args = parser.parse_args()
+
+def geturl(givenName):
+	if validators.url():
+		url, page = string.rsplit(args.PAGENAME, '/', maxsplit=1)
+		pagename=url + '/Spezial:Exportieren/' + page
+	else:
+		pagename = 'https://wiki.diehumanisten.de/wiki/index.php/Spezial:Exportieren/' + args.PAGENAME
+	if not validators.url(pagename):
+		raise Exception("This is not a valid url: " + pagename )
+	return pagename
+
+def getxmldict(url):
+	xml_request = requests.get(pagename)
+	if not xml_request.status_code == 200:
+		raise Exception("Bad status code" + str(xml_request.status_code) )
+	xml = xmltodict.parse(xml_request.text)
+	return xml
+
+def getContent(xmlDict):
+	return xmlDict['mediawiki']['page']['revision']['text']['#text']
+
+url = geturl(args.PAGENAME)
+xmlDict = getxmldict(url)
+content = getContent(xmlDict)
+content = getContent(xmlDict)
+print content
